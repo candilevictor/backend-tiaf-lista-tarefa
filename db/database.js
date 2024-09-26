daebef8ae62d195d56b2e84ffab3ddc64e706b6a
candilevictor <victorcandile@gmail.com>
2024-09-26 09:07:25 -0300

    Cadastro de Usuários e Autenticação com JWT e Bcrypt

--- db/database.js
+++ db/database.js
@@ -7,3 +7,12 @@ db.serialize(() => {
 });
 
 module.exports = db;
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role TEXT
+  )
+`);
